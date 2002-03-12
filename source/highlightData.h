49298a7c79733860e9ac689914929e36f7d1cb50
Eddy De Greef <edg@users.sourceforge.net>
2002-03-12 20:20:20 +0000

    Revised syntax-based paren matching algorithm: relaxes constraints on highlight patterns. Valid matches should no longer be missed.

--- source/highlightData.h
+++ source/highlightData.h
@@ -1,14 +1,15 @@
-/* $Id: highlightData.h,v 1.4 2001/04/06 09:49:56 amai Exp $ */
+/* $Id: highlightData.h,v 1.6.2.1 2002/03/12 20:20:20 edg Exp $ */
 patternSet *FindPatternSet(const char *langModeName);
 int LoadHighlightString(char *inString, int convertOld);
 char *WriteHighlightString(void);
 int LoadStylesString(char *inString);
 char *WriteStylesString(void);
-void EditHighlightStyles(Widget parent, char *initialStyle);
+void EditHighlightStyles(Widget parent, const char *initialStyle);
 void EditHighlightPatterns(WindowInfo *window);
 void UpdateLanguageModeMenu(void);
-int LMHasHighlightPatterns(char *languageMode);
-XFontStruct *FontOfNamedStyle(WindowInfo *window, char *styleName);
-char *ColorOfNamedStyle(char *styleName);
-int NamedStyleExists(char *styleName);
-void RenameHighlightPattern(char *oldName, char *newName);
+int LMHasHighlightPatterns(const char *languageMode);
+XFontStruct *FontOfNamedStyle(WindowInfo *window, const char *styleName);
+char *ColorOfNamedStyle(const char *styleName);
+int IndexOfNamedStyle(const char *styleName);
+int NamedStyleExists(const char *styleName);
+void RenameHighlightPattern(const char *oldName, const char *newName);
